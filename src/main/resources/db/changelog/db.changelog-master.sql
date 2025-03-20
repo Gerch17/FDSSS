c3a7526c03aaa22312a6c1f2a38e24c5c4b08be3
g.tambovtsev <g.tambovtsev@tinkoff.ru>
2025-03-20 11:20:48 +0300

    init

--- src/main/resources/db/changelog/db.changelog-master.sql
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -10,10 +10,10 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE user_roles (
-    user_id BIGINT NOT NULL,
+    client_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
-    PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES client(id) ON DELETE CASCADE,
+    PRIMARY KEY (client_id, role_id),
+    CONSTRAINT fk_user_role FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE,
     CONSTRAINT fk_role_user FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
@@ -21,5 +21,5 @@ CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     client_id BIGINT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES client(id) ON DELETE CASCADE
+    CONSTRAINT fk_user FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE
 );

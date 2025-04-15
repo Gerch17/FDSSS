6f3e0e2fcde88c3e817c4df048d888d48a837040
g.tambovtsev <g.tambovtsev@tinkoff.ru>
2025-04-15 14:55:06 +0300

    job

---
+++ src/main/java/ru/tbank/fdsspring/job/CurrencyRateJob.java
@@ -0,0 +1,24 @@
+package ru.tbank.fdsspring.job;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.tbank.fdsspring.model.CurrencyRateDto;
+import ru.tbank.fdsspring.service.CbrClient;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CurrencyRateJob {
+
+    private final CbrClient cbrClient;
+
+    @Scheduled(cron = "0 0 * * *")
+    @PostConstruct
+    public void fetchAndPrintRates() {
+        List<CurrencyRateDto> rates = cbrClient.fetchRates();
+        rates.forEach(System.out::println);
+    }
+}
